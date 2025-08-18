075c25e72642ed32783be5d173c9275378a42013
Sumit Yesade <153898079+SumitYesade22@users.noreply.github.com>
2025-08-18 07:13:29 +0530

    Time: 1 ms (4.33%), Space: 42.1 MB (30.28%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int c=0;
+        for(int i= 0 ; i<nums.length;i++){
+            if(nums[i]==val){
+                c++;
+                nums[i]='_';
+            }
+        }
+        Arrays.sort(nums);
+        return nums.length-c;
+        
+    }
+}
