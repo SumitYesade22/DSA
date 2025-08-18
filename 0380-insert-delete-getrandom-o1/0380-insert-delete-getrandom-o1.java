39f0aadd9c7fecd30217d5bf69aaec7fba96d23f
Sumit Yesade <153898079+SumitYesade22@users.noreply.github.com>
2025-08-18 22:21:55 +0530

    Time: 221 ms (5%), Space: 89.3 MB (82.76%) - LeetHub

---
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
@@ -0,0 +1,39 @@
+class RandomizedSet {
+    ArrayList<Integer> arr;
+
+    public RandomizedSet() {
+        arr=new ArrayList<>();
+        
+    }
+    
+    public boolean insert(int val) {
+        if(arr.contains(val)){
+            return false;
+        }
+        arr.add(val);
+        return true;
+        
+    }
+    
+    public boolean remove(int val) {
+        
+        if(!arr.contains(val)){
+            return false;
+        }
+        arr.remove(Integer.valueOf(val));
+        return true;
+    }
+    
+    public int getRandom() {
+        Random ran = new Random();
+        return (arr.get(ran.nextInt(arr.size())));
+    }
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
