ca922805ea7f530cd1939a4e44422d00543e2418
Sumit Yesade <153898079+SumitYesade22@users.noreply.github.com>
2025-08-31 20:26:30 +0530

    Time: 5 ms (81.83%), Space: 82.4 MB (42.06%) - LeetHub

---
+++ 2299-merge-nodes-in-between-zeros/2299-merge-nodes-in-between-zeros.java
@@ -0,0 +1,23 @@
+class Solution {
+    public ListNode mergeNodes(ListNode head) {
+        ListNode temp = head.next;  
+        int sum = 0;
+        
+        ListNode dummy = new ListNode(0); 
+        ListNode curr = dummy;
+        
+        while (temp!= null) {
+            if (temp.val == 0) {
+               
+                curr.next = new ListNode(sum);
+                curr = curr.next;
+                sum = 0; 
+            } else {
+                sum += temp.val; 
+            }
+            temp = temp.next;
+        }
+        
+        return dummy.next; // skip dummy
+    }
+}
