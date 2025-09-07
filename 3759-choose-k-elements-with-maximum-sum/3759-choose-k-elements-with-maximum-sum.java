74c4809fde9e2917be0764501e42d199113a6485
Sumit Yesade <153898079+SumitYesade22@users.noreply.github.com>
2025-09-08 02:58:32 +0530

    Time: 158 ms (78.38%), Space: 71.5 MB (39.46%) - LeetHub

--- 3759-choose-k-elements-with-maximum-sum/3759-choose-k-elements-with-maximum-sum.java
+++ 3759-choose-k-elements-with-maximum-sum/3759-choose-k-elements-with-maximum-sum.java
@@ -1,20 +1,20 @@
 class Solution {
     public long[] findMaxSum(int[] nums1, int[] nums2, int k) {
         int n = nums1.length;
-        int[][] vec = new int[n][3]; //vec[i] = {nums1[i], i, nums2[i]}
-        for (int i = 0; i < n; i++) { //O(n)
+        int[][] vec = new int[n][3]; 
+        for (int i = 0; i < n; i++) { 
             vec[i][0] = nums1[i];
             vec[i][1] = i;
             vec[i][2] = nums2[i];
         }
 
-        Arrays.sort(vec, (a, b) -> Integer.compare(a[0], b[0])); //O(nlogn)
+        Arrays.sort(vec, (a, b) -> Integer.compare(a[0], b[0])); 
 
         long[] result = new long[n];
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         long sum = 0;
 
-        for (int i = 0; i < n; i++) { //O(nlogk)
+        for (int i = 0; i < n; i++) { 
             if (i > 0 && vec[i - 1][0] == vec[i][0]) {
                 long ans = result[vec[i - 1][1]];
                 result[vec[i][1]] = ans;
