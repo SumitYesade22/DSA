584894b928d188d2c6c4e1d3764255f8845706cb
Sumit Yesade <153898079+SumitYesade22@users.noreply.github.com>
2025-09-09 12:54:44 +0530

    Time: 22 ms (25.64%), Space: 50.5 MB (21.81%) - LeetHub

---
+++ 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.java
@@ -0,0 +1,21 @@
+class KthLargest {
+    private PriorityQueue<Integer> pq;  
+    private int k;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        pq = new PriorityQueue<>(); 
+
+        for (int n : nums) {
+            add(n);
+        }
+    }
+    
+    public int add(int val) {
+        pq.offer(val);
+        if (pq.size() > k) {
+            pq.poll(); 
+        }
+        return pq.peek(); 
+    }
+}
