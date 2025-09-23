4fa37e9ff8d4e28a266c5154028653c8a39b5eb7
Sumit Yesade <153898079+SumitYesade22@users.noreply.github.com>
2025-09-23 13:42:35 +0530

    Time: 30 ms (86.62%), Space: 45.8 MB (65.81%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int distributeCandies(int[] candyType) {
+      
+        Set<Integer> set = new HashSet<>();
+        for (int candy : candyType) {
+            set.add(candy);
+        }
+        
+        
+        int maxEat = candyType.length / 2;
+        
+       
+        return Math.min(set.size(), maxEat);
+    }
+}
