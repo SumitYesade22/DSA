2384bdb360d10c52d22341eb7078623dc0a00e05
Sumit Yesade <153898079+SumitYesade22@users.noreply.github.com>
2025-10-03 18:01:31 +0530

    Time: 18 ms (41.68%), Space: 44.7 MB (47.93%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int n=coins.length;
+        if(amount==0){
+            return 0;
+        }
+        int dp[]=new int[amount+1];
+        dp[0]=0;
+        for(int i=1;i<=amount;i++){
+            dp[i]=Integer.MAX_VALUE;
+            for(int c:coins){
+                if(c<=i && dp[i-c]!=Integer.MAX_VALUE){
+                    dp[i]=Math.min(dp[i],dp[i-c]+1);
+                }
+            }
+        }
+        
+     return dp[amount]==Integer.MAX_VALUE?-1:dp[amount];   
+    }
+}
