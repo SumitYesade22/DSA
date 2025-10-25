438079cea16d148fe0be6efb4da34c9cf25b29ba
Sumit Yesade <153898079+SumitYesade22@users.noreply.github.com>
2025-10-26 03:21:13 +0530

    Time: 4 ms (95.52%), Space: 43.9 MB (34.94%) - LeetHub

---
+++ 0987-reveal-cards-in-increasing-order/0987-reveal-cards-in-increasing-order.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] deckRevealedIncreasing(int[] deck) {
+        int n = deck.length;
+        
+        Queue<Integer> que = new LinkedList<>();
+        int[] result = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            que.offer(i);
+        }
+        
+        Arrays.sort(deck);
+        
+        for (int i = 0; i < n; i++) {
+            int idx = que.poll();
+            result[idx] = deck[i];
+            
+            if (!que.isEmpty()) {
+                que.offer(que.peek());
+                que.poll();
+            }
+        }
+        
+        return result;
+    }
+}
