59c840cba2e0ea01de7ef60a108c6813285d5113
Sumit Yesade <153898079+SumitYesade22@users.noreply.github.com>
2025-11-01 22:53:25 +0530

    Time: 1 ms (99.75%), Space: 50.4 MB (9.12%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -0,0 +1,25 @@
+class Solution {
+    private int MPS(int[][] grid, int i, int j, int m, int n, int[][] t) {
+        if (i == m - 1 && j == n - 1)
+            return t[i][j] = grid[i][j];
+        if (t[i][j] != -1)
+            return t[i][j];
+        if (i == m - 1) { 
+            return t[i][j] = grid[i][j] + MPS(grid, i, j + 1, m, n, t);
+        } else if (j == n - 1) { 
+            return t[i][j] = grid[i][j] + MPS(grid, i + 1, j, m, n, t);
+        } else {
+            return t[i][j] = grid[i][j] + Math.min(MPS(grid, i + 1, j, m, n, t), MPS(grid, i, j + 1, m, n, t));
+        }
+    }
+
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] t = new int[m][n];
+        for (int[] row : t) {
+            Arrays.fill(row, -1);
+        }
+        return MPS(grid, 0, 0, m, n, t);
+    }
+}
