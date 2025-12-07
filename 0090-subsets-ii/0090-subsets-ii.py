5c6b0b4165084fb2603f350967008f67c64a3af9
Sumit Yesade <153898079+SumitYesade22@users.noreply.github.com>
2025-12-07 12:26:38 +0530

    Time: 4 ms (12.46%), Space: 18.1 MB (35.7%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()                     
+        res = set()
+        self.solve(0, nums, [], res)
+        return [list(t) for t in res]   
+
+    def solve(self, i, nums, temp, res):
+        if i == len(nums):
+            res.add(tuple(temp))       
+            return
+        
+        
+        self.solve(i + 1, nums, temp + [nums[i]], res)
+
+        
+        self.solve(i + 1, nums, temp, res)
+
+        
