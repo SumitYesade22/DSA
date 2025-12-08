7f8a419d45f0eea5988d288858eabff89a7bb753
Sumit Yesade <153898079+SumitYesade22@users.noreply.github.com>
2025-12-08 20:42:06 +0530

    Time: 11 ms (83.1%), Space: 20.5 MB (95.43%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        mp={}
+        for s in strs:
+            key="".join(sorted(s))
+            if key in mp:
+                mp[key].append(s)
+            else:
+                mp[key]=[s]
+                # mp[key].append(s)
+        return list(mp.values())
+        a
