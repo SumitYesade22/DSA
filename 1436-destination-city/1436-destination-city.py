a114a6f472f4472b0248867a7e68f728643d8fdf
Sumit Yesade <153898079+SumitYesade22@users.noreply.github.com>
2026-01-09 10:33:59 +0530

    Time: 2 ms (24.64%), Space: 19.4 MB (9.94%) - LeetHub

---
+++ 1436-destination-city/1436-destination-city.py
@@ -0,0 +1,9 @@
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str:
+        source = set()
+        dest = set()
+        for l in paths:
+            source.add(l[0])
+            dest.add(l[1])
+        return list(dest - source)[0]
+        
