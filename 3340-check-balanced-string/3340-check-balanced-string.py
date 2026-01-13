1e341641da2d6189b40b582872db34c21fe2464a
Sumit Yesade <153898079+SumitYesade22@users.noreply.github.com>
2026-01-13 16:03:07 +0530

    Time: 0 ms (100%), Space: 19.5 MB (7.67%) - LeetHub

---
+++ 3340-check-balanced-string/3340-check-balanced-string.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isBalanced(self, num: str) -> bool:
+        evensum=0
+        oddsum=0
+        for i in range(len(num)):
+            if i%2==0:
+                evensum+=int(num[i])
+            else:
+                oddsum+=int(num[i])
+        return evensum==oddsum
+        
