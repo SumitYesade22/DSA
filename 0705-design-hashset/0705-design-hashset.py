1aa7b5b14a18967134772684241dd35d213272a6
Sumit Yesade <153898079+SumitYesade22@users.noreply.github.com>
2026-02-02 00:29:16 +0530

    Time: 60 ms (44.05%), Space: 25.7 MB (28.32%) - LeetHub

---
+++ 0705-design-hashset/0705-design-hashset.py
@@ -0,0 +1,22 @@
+class MyHashSet:
+
+    def __init__(self):
+        self.numBuckets = 15000
+        self.buckets = [[] for _ in range(self.numBuckets)]
+
+    def _hash(self, key: int) -> int:
+        return key % self.numBuckets
+
+    def add(self, key: int) -> None:
+        index = self._hash(key)
+        if key not in self.buckets[index]:
+            self.buckets[index].append(key)
+
+    def remove(self, key: int) -> None:
+        index = self._hash(key)
+        if key in self.buckets[index]:
+            self.buckets[index].remove(key)
+
+    def contains(self, key: int) -> bool:
+        index = self._hash(key)
+        return key in self.buckets[index]
